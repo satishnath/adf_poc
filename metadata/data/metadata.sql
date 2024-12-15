4912b05d7799b4e82096bd7b65099104f57bec0b
vekontib <54058286+vekontib@users.noreply.github.com>
2024-12-15 18:17:25 +0100

    Update metadata.sql

--- metadata/data/metadata.sql
+++ metadata/data/metadata.sql
@@ -96,14 +96,14 @@ THEN DELETE
 MERGE INTO  [metadata].[Job]    AS  [TRG]
 USING   (
     SELECT
-        1               AS  [JobId]
-    ,   'Dummy Job'     AS  [JobName]
-    ,   1               AS  [EnvironmentId]
-    ,   GETDATE()       AS  [InsertedDate]
-    ,   CURRENT_USER    AS  [InsertedBy]
-    ,   GETDATE()       AS  [UpdatedDate]
-    ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
+        1                           AS  [JobId]
+    ,   'GCP Bucket File Copy'      AS  [JobName]
+    ,   1                           AS  [EnvironmentId]
+    ,   GETDATE()                   AS  [InsertedDate]
+    ,   CURRENT_USER                AS  [InsertedBy]
+    ,   GETDATE()                   AS  [UpdatedDate]
+    ,   CURRENT_USER                AS  [UpdatedBy]
+    ,   1                           AS  [IsActive]
 
 )
                                         AS  [SRC]
@@ -129,14 +129,14 @@ THEN DELETE
 MERGE INTO  [metadata].[SourceSystem]    AS  [TRG]
 USING   (
     SELECT
-        1               AS  [SourceSystemId]
-    ,   'Dummy SS'      AS  [SourceSystemName]
-    ,   1               AS  [EnvironmentId]
-    ,   GETDATE()       AS  [InsertedDate]
-    ,   CURRENT_USER    AS  [InsertedBy]
-    ,   GETDATE()       AS  [UpdatedDate]
-    ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
+        1                           AS  [SourceSystemId]
+    ,   'GCP Bucket File Copy'      AS  [SourceSystemName]
+    ,   1                           AS  [EnvironmentId]
+    ,   GETDATE()                   AS  [InsertedDate]
+    ,   CURRENT_USER                AS  [InsertedBy]
+    ,   GETDATE()                   AS  [UpdatedDate]
+    ,   CURRENT_USER                AS  [UpdatedBy]
+    ,   1                           AS  [IsActive]
 
 )
                                         AS  [SRC]
@@ -163,16 +163,16 @@ THEN DELETE
 MERGE INTO  [metadata].[SourceSystemTenant]    AS  [TRG]
 USING   (
     SELECT
-        1               AS  [SourceSystemTenantId]
-    ,   'Dummy SST'     AS  [SourceSystemTenantName]
-    ,   1               AS  [SourceSystemId]
-    ,   GETDATE()       AS  [InsertedDate]
-    ,   CURRENT_USER    AS  [InsertedBy]
-    ,   GETDATE()       AS  [UpdatedDate]
-    ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
+        1                           AS  [SourceSystemTenantId]
+    ,   'GCP Bucket File Copy'      AS  [SourceSystemTenantName]
+    ,   1                           AS  [SourceSystemId]
+    ,   GETDATE()                   AS  [InsertedDate]
+    ,   CURRENT_USER                AS  [InsertedBy]
+    ,   GETDATE()                   AS  [UpdatedDate]
+    ,   CURRENT_USER                AS  [UpdatedBy]
+    ,   1                           AS  [IsActive]
 
-UNION ALL SELECT 2, 'Dummy SST2', 1, GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+--UNION ALL SELECT 2, 'Dummy SST2', 1, GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
 )
                                         AS  [SRC]
 ON      [TRG].[SourceSystemTenantId]   = [SRC].[SourceSystemTenantId]
@@ -228,16 +228,16 @@ THEN DELETE
 MERGE INTO  [metadata].[SourceSystemEntity]    AS  [TRG]
 USING   (
     SELECT
-        1               AS  [SourceSystemEntityId]
-    ,   'Dummy SSE'     AS  [SourceSystemEntityName]
-    ,   1               AS  [SourceSystemId]
-    ,   GETDATE()       AS  [InsertedDate]
-    ,   CURRENT_USER    AS  [InsertedBy]
-    ,   GETDATE()       AS  [UpdatedDate]
-    ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
+        1                                       AS  [SourceSystemEntityId]
+    ,   'GCP Bucket File Copy - Folder 1'       AS  [SourceSystemEntityName]
+    ,   1                                       AS  [SourceSystemId]
+    ,   GETDATE()                               AS  [InsertedDate]
+    ,   CURRENT_USER                            AS  [InsertedBy]
+    ,   GETDATE()                               AS  [UpdatedDate]
+    ,   CURRENT_USER                            AS  [UpdatedBy]
+    ,   1                                       AS  [IsActive]
 
-UNION ALL SELECT 2, 'Dummy SSE2', 1, GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 2, 'GCP Bucket File Copy - Folder 2', 1, GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
 )
                                         AS  [SRC]
 ON      [TRG].[SourceSystemEntityId]   = [SRC].[SourceSystemEntityId]
@@ -270,9 +270,25 @@ USING   (
     ,   CURRENT_USER    AS  [InsertedBy]
     ,   GETDATE()       AS  [UpdatedDate]
     ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
+    ,   0               AS  [IsActive]
 
-UNION ALL SELECT 1, 'Key2', 'Value', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+-- GCP Bucket File Copy - Folder 1
+UNION ALL SELECT 1, 'SourceGcpBucketName', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 1, 'SourceGcpBucketDirectoryPath', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 1, 'SourceGcpBucketAccessKeyId', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 1, 'SourceGcpBucketSecretAccessKeySecretName', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 1, 'SourceGcpBucketFilePath', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 1, 'Adls2Container', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 1, 'Adls2FileName', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+
+-- GCP Bucket File Copy - Folder 2
+UNION ALL SELECT 2, 'SourceGcpBucketName', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 2, 'SourceGcpBucketDirectoryPath', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 2, 'SourceGcpBucketAccessKeyId', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 2, 'SourceGcpBucketSecretAccessKeySecretName', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 2, 'SourceGcpBucketFilePath', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 2, 'Adls2Container', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
+UNION ALL SELECT 2, 'Adls2FileName', 'PLACEHOLDER', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
 )
                                         AS  [SRC]
 ON      [TRG].[SourceSystemEntityId]   = [SRC].[SourceSystemEntityId]
@@ -289,109 +305,6 @@ THEN UPDATE
 WHEN NOT MATCHED BY TARGET
 THEN INSERT ([SourceSystemEntityId], [Key], [Value], [InsertedDate], [InsertedBy], [UpdatedDate], [UpdatedBy], [IsActive])
 VALUES ([SRC].[SourceSystemEntityId], [SRC].[Key], [SRC].[Value], [SRC].[InsertedDate], [SRC].[InsertedBy], [SRC].[UpdatedDate], [SRC].[UpdatedBy], [SRC].[IsActive])
-
-WHEN NOT MATCHED BY SOURCE
-THEN DELETE
-;GO
-
-
-MERGE INTO  [metadata].[ModelEntity]    AS  [TRG]
-USING   (
-    SELECT
-        1               AS  [ModelEntityId]
-    ,   'ModelName'     AS  [ModelEntityName]
-    ,   2               AS  [EnvironmentId]
-    ,   GETDATE()       AS  [InsertedDate]
-    ,   CURRENT_USER    AS  [InsertedBy]
-    ,   GETDATE()       AS  [UpdatedDate]
-    ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
-
-UNION ALL SELECT 1, 'ModelName2', 2, GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
-)
-                                        AS  [SRC]
-ON      [TRG].[ModelEntityId]   = [SRC].[ModelEntityId]
-
-WHEN MATCHED
-THEN UPDATE
-    SET
-        [TRG].[ModelEntityName]     =   [SRC].[ModelEntityName]
-    ,   [TRG].[EnvironmentId]       =   [SRC].[EnvironmentId]
-    ,   [TRG].[UpdatedBy]           =   [SRC].[UpdatedBy]
-    ,   [TRG].[UpdatedDate]         =   [SRC].[UpdatedDate]
-    ,   [TRG].[IsActive]            =   [SRC].[IsActive]
-
-WHEN NOT MATCHED BY TARGET
-THEN INSERT ([ModelEntityId], [ModelEntityName], [EnvironmentId], [InsertedDate], [InsertedBy], [UpdatedDate], [UpdatedBy], [IsActive])
-VALUES ([SRC].[ModelEntityId], [SRC].[ModelEntityName], [SRC].[EnvironmentId], [SRC].[InsertedDate], [SRC].[InsertedBy], [SRC].[UpdatedDate], [SRC].[UpdatedBy], [SRC].[IsActive])
-
-WHEN NOT MATCHED BY SOURCE
-THEN DELETE
-;GO
-
-
-MERGE INTO  [metadata].[ModelEntityParameters]    AS  [TRG]
-USING   (
-    SELECT
-        1               AS  [ModelEntityId]
-    ,   'Key'           AS  [Key]
-    ,   'Value'         AS  [Value]
-    ,   GETDATE()       AS  [InsertedDate]
-    ,   CURRENT_USER    AS  [InsertedBy]
-    ,   GETDATE()       AS  [UpdatedDate]
-    ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
-
-UNION ALL SELECT 1, 'Key2', 'Value', GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
-)
-                                        AS  [SRC]
-ON      [TRG].[ModelEntityId]   = [SRC].[ModelEntityId]
-    AND [TRG].[Key]             = [SRC].[Key]
-
-WHEN MATCHED
-THEN UPDATE
-    SET
-        [TRG].[Value]           =   [SRC].[Value]
-    ,   [TRG].[UpdatedBy]       =   [SRC].[UpdatedBy]
-    ,   [TRG].[UpdatedDate]     =   [SRC].[UpdatedDate]
-    ,   [TRG].[IsActive]        =   [SRC].[IsActive]
-
-WHEN NOT MATCHED BY TARGET
-THEN INSERT ([ModelEntityId], [Key], [Value], [InsertedDate], [InsertedBy], [UpdatedDate], [UpdatedBy], [IsActive])
-VALUES ([SRC].[ModelEntityId], [SRC].[Key], [SRC].[Value], [SRC].[InsertedDate], [SRC].[InsertedBy], [SRC].[UpdatedDate], [SRC].[UpdatedBy], [SRC].[IsActive])
-
-WHEN NOT MATCHED BY SOURCE
-THEN DELETE
-;GO
-
-
-MERGE INTO  [metadata].[SourceSystemEntityModelEntity]    AS  [TRG]
-USING   (
-    SELECT
-        1               AS  [ModelEntityId]
-    ,   1               AS  [SourceSystemEntityId]
-    ,   GETDATE()       AS  [InsertedDate]
-    ,   CURRENT_USER    AS  [InsertedBy]
-    ,   GETDATE()       AS  [UpdatedDate]
-    ,   CURRENT_USER    AS  [UpdatedBy]
-    ,   1               AS  [IsActive]
-
-UNION ALL SELECT 2, 2, GETDATE(), CURRENT_USER, GETDATE(), CURRENT_USER, 1
-)
-                                        AS  [SRC]
-ON      [TRG].[ModelEntityId]           = [SRC].[ModelEntityId]
-    AND [TRG].[SourceSystemEntityId]    = [SRC].[SourceSystemEntityId]
-
-WHEN MATCHED
-THEN UPDATE
-    SET
-        [TRG].[UpdatedBy]       =   [SRC].[UpdatedBy]
-    ,   [TRG].[UpdatedDate]     =   [SRC].[UpdatedDate]
-    ,   [TRG].[IsActive]        =   [SRC].[IsActive]
-
-WHEN NOT MATCHED BY TARGET
-THEN INSERT ([ModelEntityId], [SourceSystemEntityId], [InsertedDate], [InsertedBy], [UpdatedDate], [UpdatedBy], [IsActive])
-VALUES ([SRC].[ModelEntityId], [SRC].[SourceSystemEntityId], [SRC].[InsertedDate], [SRC].[InsertedBy], [SRC].[UpdatedDate], [SRC].[UpdatedBy], [SRC].[IsActive])
 
 WHEN NOT MATCHED BY SOURCE
 THEN DELETE
